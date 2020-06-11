89fc0fa3077dd125562511a4915eb6fbf36597ff
IvyCodes <ivy@cs.stanford.edu>
2020-06-10 18:19:02 -0700

    added more data, removed unused files

--- scraper/reddit_praw_scrapper.py
+++ scraper/reddit_praw_scrapper.py
@@ -22,17 +22,18 @@ api = PushshiftAPI(reddit)
 
 
 
-end_epoch=int(dt.datetime(2019, 1, 2).timestamp())
-start_epoch=int(dt.datetime(2019, 1, 1).timestamp())
+end_epoch=int(dt.datetime(2016, 1, 2).timestamp())
+start_epoch=int(dt.datetime(2016, 1, 1).timestamp())
 counter = 0
 
-for i in range(365):
+for i in range(365*4):
     for post in list(api.search_submissions(before=end_epoch, after = start_epoch, subreddit = 'AmItheAsshole', limit = 100, filter = ['id', 'title', 'score',  'num_comments', 'selftext', 'created', 'link_flair_text'])):
         post = [[post.id, post.title, post.score,  post.num_comments, post.selftext, post.created, post.link_flair_text]]
         aita_df = pd.DataFrame(post, columns = ['id', 'title', 'score', 'num_comments', 'body', 'created', 'flair'])
         # print(aita_df)
         aita_df.to_csv('aita.csv', mode = 'a+', index = None, header = None)
         counter += 1
+    print('day ' + str(i) + ':\n')
     print('processed ' + str(counter) + ' entries')
     start_epoch +=  86400
     end_epoch +=  86400
